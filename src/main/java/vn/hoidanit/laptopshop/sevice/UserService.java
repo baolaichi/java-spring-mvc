23d9ef0551b202db0f0828b5f52254b17e2fb815
baolaichi <131328468+baolaichi@users.noreply.github.com>
2025-04-01 09:47:50 +0700

    update

---
+++ src/main/java/vn/hoidanit/laptopshop/sevice/UserService.java
@@ -0,0 +1,12 @@
+package vn.hoidanit.laptopshop.sevice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String HandlleHello(){
+        return "Hello world";
+    }
+    
+}
